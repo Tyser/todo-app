d1355f809f726021dec7d3428cb487712ee62c18
Tyler Burgess <tyster23@gmail.com>
2015-10-29 08:44:41 -0600

    Here is my ToDo app
    worked in class work, I have questions regarding my own project.

---
+++ src/scripts/app.js
@@ -0,0 +1,17 @@
+angular
+.module('TodoApp', [
+  'ngRoute',
+  'LoginController',
+])
+
+.config([
+  '$routeProvider',
+  function($routeProvider) {
+    $routeProvider
+      .when('/login', {
+        templateUrl: '/partials/login-controller.html',
+        controller: 'LoginController',
+      })
+      .otherwise('/login');
+    }
+]);
